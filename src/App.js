b1fad5d310f409263de6a6ce608f378293404944
Andrew Thornton <awjdthornton@gmail.com>
2019-09-09 11:46:55 -0700

    turned onClick alert into a method

--- src/App.js
+++ src/App.js
@@ -152,6 +152,10 @@ class App extends Component {
     })
   }
   
+  alertDiff = (record) => {
+    alert(record.TEAM_ABR + " won " + (record.REG_W_PCT - record.PLYOFF_W_PCT) + "% fewer of their games during the playoffs.")
+  }
+  
   render() {
     return (
         <div className="Viz">
@@ -171,7 +175,7 @@ class App extends Component {
               {
                 this.state.mergedData.map(record => (
                   (this.state.conference === "All" || this.state.conference === record.TEAM_CONF) && (
-                    <div className="VizItem" onClick={() => alert(record.TEAM_ABR + " won " + (record.REG_W_PCT - record.PLYOFF_W_PCT) + "% fewer of their games during the playoffs.")}>
+                    <div className="VizItem" onClick={() => this.alertDiff(record)}>
                       <div className="VizData VizRegular" style={{height: record.REG_W_PCT + "%"}}><label>{record.REG_W_PCT}</label></div>
                       <div className="VizData VizPlayoff" style={{height: record.PLYOFF_W_PCT + "%"}}><label>{record.PLYOFF_W_PCT}</label></div>
                       <div className="VizLabel">{record.TEAM_ABR}</div>
